07e0b3b69a067822c814ff7b231783f84c77be4c
pooyaj21 <105147486+pooyaj21@users.noreply.github.com>
2023-06-12 17:53:24 +0430

    main fix

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,6 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<MenuItem> mainMenu = new ArrayList<>();
-        mainMenu.add(new Add());
-        mainMenu.add(new Show());
-        mainMenu.add(new Edit());
-        mainMenu.add(new SecondMenu());
-        MenuMaker.menuMaker(mainMenu);
+       new MainMenu().performAction();
     }
 }
